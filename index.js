652c67a64a136409a6d19aa310aaa0af80007fb1
rorofit <root@rodrigo-reb-ltm.internal.salesforce.com>
2016-12-06 16:21:59 -0500

    added hasInterview intent and jsforce

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.error = function( exception, request, response ) {
 	response.say( 'Sorry an error occured ' + error.message);
 };
 
+/*
 app.intent('sayNumber',
   {
     "slots":{"number":"NUMBER"}
@@ -29,6 +30,67 @@ app.intent('sayNumber',
   function(request,response) {
     var number = request.slot('number');
     response.say("You asked for the number "+number);
+  }
+);
+*/
+
+
+
+app.intent('haveInterview',
+  {
+    "slots":{"date":"DATE"}
+	,"utterances":[ 
+		"do I have any interviews {date}",
+		"which interviews I have {date}",
+		"are there any interviews {date}",
+		"any interviews {date}"]
+  },
+  function(request,response) {
+  	var dateSpoken = request.slot('date');
+  	var records = [];
+  	var jsforce = require('jsforce');
+	var conn = new jsforce.Connection({
+  // you can change loginUrl to connect to sandbox or prerelease env.
+    loginUrl : 'https://login.salesforce.com'
+});
+	conn.login('mycs1@sparkle.cstest', 'Alexa01!', function(err, userInfo) {
+  if (err) { return console.error(err); }
+  // Now you can get the access token and instance URL information.
+  // Save them to establish connection next time.
+  console.log(conn.accessToken);
+  console.log(conn.instanceUrl);
+  // logged in user property
+  console.log("User ID: " + userInfo.id);
+  console.log("Org ID: " + userInfo.organizationId);
+
+  // SOQL:
+  
+
+  
+	var query = conn.query("SELECT Candidate__c, Candidate__r.Name,Id,InterViewDateValue__c,Interview_Date__c,Interview_Type__c,Name FROM Interview__c where InterViewDateValue__c = " + dateSpoken)
+	  .on("record", function(record) {
+	    records.push(record);
+	  })
+	  .on("end", function() {
+	    console.log("total in database : " + query.totalSize);
+	    console.log("total fetched : " + query.totalFetched);
+	    console.log("records " + records);
+	    response.say("You have "+ query.totalFetched +  dateSpoken);
+	  })
+	  .on("error", function(err) {
+	    console.error(err);
+	  })
+	  .run({ autoFetch : true, maxFetch : 4000 }); // synonym of Query#execute();
+
+
+
+  // ...
+});
+
+    //var number = request.slot('number');
+    //console.log("records " + records);
+
+    //response.say("You have "+ dateSpoken);
   }
 );
 
